562ae402ea3d0004efc34f41d7ec4f83086f0eaf
BlooSkyd <clment2402@gmail.com>
2022-11-09 09:19:27 +0100

    Optimisation de la génération ; Optimisation plus poussée pour la génération du bois ; Révision du format pour les proportions des obstables ; Ajout de couleur de debug pour l'affichage console ;

---
+++ src/Display.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+public class Display {
+    private static int HEIGHT;
+    private static int WIDTH;
+
+    public void setWindowSize(int h, int w) {
+        HEIGHT = h;
+        WIDTH = w;
+    }
+
+}
